89b30d53ee6ff373007401d1d22b1b7d0b513cff
ericcapiz <ericcapiz@gmail.com>
2021-03-17 22:02:25 -0500

    added motd

--- api/server.js
+++ api/server.js
@@ -8,5 +8,10 @@ const server = express();
 server.use(helmet());
 
 server.use('/api', apiRouter);
+//message of the day
+server.get('/motd', (req,res)=>{
+    const motd = process.env.MOTD || "Hello World";
+    res.status(200).json({motd: motd})
+})
 
 module.exports = server;
